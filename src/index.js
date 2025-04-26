a40062748946d520eea49636207fad5106a56cf4
alexmara123 <mara.alexandru@gmail.com>
2025-04-26 22:53:53 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/my-project
+  /public
+    index.html      ← contains <div id="root"></div>
+  /src
+    index.js        ← the file you create
+    App.js
+    style.css
+    script.js       (if used)
+  package.json      ← at root, with build scripts
